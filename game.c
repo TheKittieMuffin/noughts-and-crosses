bffc447acdca779dd5fa2c0ca5101260636015ed
TheKittieMuffin <sergeantstaramazing@gmail.com>
2023-03-30 18:54:54 +0100

    user input

--- game.c
+++ game.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+char board[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+int first;
+int second;
+bool check = false;
+
+int userInput(void);
 
 int main(int argc, char* argv[]) {
-	printf("Hello World!");
+	do {
+		first = userInput();
+		second = userInput();
+		board[first][second] = 'X';
+		for (int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++) {
+				printf("[%c]", board[i][j]);
+			}
+			printf("\n");
+		}
+
+		//if the board is filled up, check = false and game ends
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (!isalpha(board[i][j])) {
+					check = true;
+					j = 3;
+					i = 3;
+				} else {
+					check = false;
+				}
+			}
+		}
+	} while (check == true);
 	return EXIT_SUCCESS;
+}
+
+int userInput(void) {
+	int input = -1;
+	do {
+		printf("please enter: ");
+		scanf("%i", &input);
+	} while ((input < 0) || (input > 2));
+	return input;
 }
